8ad067bba3ebe4ccba8fbd4f06e0d201577ae41f
jyzene <67204491+jyzene@users.noreply.github.com>
2022-03-04 17:47:31 -0600

    Add files via upload

---
+++ pages/script/typesale.js
@@ -0,0 +1,6 @@
+const typed4 = new Typed('.typed4', {
+  strings: ['Sale.','¡Revisa las ofertas!'],
+  typeSpeed: 65,
+  backSpeed: 65,
+  loop: true,
+});
---
+++ pages/script/typestore.js
@@ -0,0 +1,4 @@
+const typed5 = new Typed('.typed5', {
+  strings: ['Joyeria y Accesorios'],
+  typeSpeed: 65,
+});
